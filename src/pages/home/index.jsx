5f88cddbb5e883af7a8ce05966718e722a43bf80
KyleYung <kylehyung@gmail.com>
2025-01-06 18:11:40 -0500

    completed home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import './styles.css';
 
 const Home=()=>{
     return (
-        <div>
-            Home Page
-        </div>
+        <section className="home" id="home">
+            <div className="home-container">
+                <h1 className="home-heading">Hey, I'm<span> Kyle</span></h1>
+                <p className="home-description">Nice to meet you</p>
+            </div>
+        </section>
     )
 }
 
