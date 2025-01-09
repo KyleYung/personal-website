08e7dafc49f4ad776bff33235e564c2fadea2472
KyleYung <kylehyung@gmail.com>
2025-01-09 18:07:59 -0500

    created base for about and experience pages

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import './styles.css';
 
 const About=()=>{
     return (
-        <div>
-            About Page
+        <div class="about" id="about">
+            <h1 className="about-header">About Me</h1>
+            <div class="about-content">
+                <img src="IMG_5405.JPG" class="about-img" alt="Image of Me"/>
+                <p className="about-bio">I am currently a senior at Boston University and majoring in Computer Science.</p>
+            </div>
         </div>
     )
 }
--- src/pages/experience/index.jsx
+++ src/pages/experience/index.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import './styles.css';
 
 const Experience=()=>{
     return (
-        <div>
-            Experience Page
+        <div className="timeline">
+
+            <div className="timeline-container left">
+                <img className = "timeline-img" src="beaver.png" alt=""/>
+                <div className="timeline-item">
+                    <h2>Beaver Country Day School</h2>
+                    <h4>IT Technician Intern</h4>
+                    <small>March 2021 - June 2021</small>
+                    <p>blah blah blah</p>
+                    <span className="left-arrow"></span>
+                </div>
+            </div>
+
+            <div className="timeline-container right">
+                <img className = "timeline-img" src="rf2.jpg" alt=""/>
+                <div className="timeline-item">
+                    <h2>Rodman Ford</h2>
+                    <h4>Software Engineer Intern</h4>
+                    <small>May 2022 - August 2022</small>
+                    <p>blah blah blah</p>
+                    <span className="right-arrow"></span>
+                </div>
+            </div>
+
+            <div className="timeline-container left">
+                <img className = "timeline-img" src="rf.jpg" alt=""/>
+                <div className="timeline-item">
+                    <h2>Rodman Ford</h2>
+                    <h4>IT Technician Intern</h4>
+                    <small>May 2024 - August 2024</small>
+                    <p>blah blah blah</p>
+                    <span className="left-arrow"></span>
+                </div>
+            </div>
+
+            <div className="timeline-container right">
+                <img className = "timeline-img" src="bu.png" alt=""/>
+                <div className="timeline-item">
+                    <h2>Boston University</h2>
+                    <h4>Course Assistant</h4>
+                    <small>September 2024 - December 2024</small>
+                    <p>blah blah blah</p>
+                    <span className="right-arrow"></span>
+                </div>
+            </div>
+
         </div>
     )
 }
